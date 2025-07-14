d1e3b5a1f9df606cbebcf71faba815e6140781a9
Michael Meffie <mmeffie@sinenomine.net>
2025-07-14 09:14:18 -0400

    tests: Add acl keyword tests

--- tests/keywords/test_acl.py
+++ tests/keywords/test_acl.py
@@ -1,101 +1,286 @@
 # Copyright (c) 2025, Sine Nomine Associates
 # See LICENSE
 
-from OpenAFSLibrary.keywords.acl import normalize, AccessControlList
+import pytest
 
-import pprint
+from OpenAFSLibrary.keywords.acl import (
+    normalize,
+    parse,
+    AccessControlList,
+    _ACLKeywords,
+)
 
+keywords = _ACLKeywords()
 
-def test_normalize():
-    cases = [
-        ("", ""),
-        ("r", "r"),
-        ("lr", "rl"),
-        ("rlidwka", "rlidwka"),
-        ("adiklrw", "rlidwka"),
-        ("abcd", None),
-    ]
-    for x, y in cases:
-        try:
-            z = "".join(normalize(list(x)))
-        except Exception:
-            assert y is None, "expected exception: x='%s'" % (x)
-        if y:
-            assert z == y, "expected='%s', got='%s'" % (y, z)
+#
+# Tests for Internal helper functions.
+#
 
 
-def test_acl_add():
-    cases = [
-        "system:administrators rlidwka",
-        "system:anyuser rl",
-        "user1 rl",
-        "user2 rl",
-        "user2 rwl",
-        "user2 -l",
-        "user3 +rlidwk",
-        "user4 none",
-        "user5 read",
-        "user6 write",
-        "user7 -write",
-    ]
-    expected = """\
-{'system:administrators': ('rlidwka', ''),
- 'system:anyuser': ('rl', ''),
- 'user1': ('rl', ''),
- 'user2': ('rlw', 'l'),
- 'user3': ('rlidwk', ''),
- 'user5': ('rl', ''),
- 'user6': ('rlidwk', ''),
- 'user7': ('', 'rlidwk')}\
-"""
-    a = AccessControlList()
-    for case in cases:
-        name, rights = case.split()
-        a.add(name, rights)
-
-    got = pprint.pformat(a.acls)
+@pytest.mark.parametrize(
+    "value, expected",
+    [
+        (list(), list()),
+        (list("r"), list("r")),
+        (list("lr"), list("rl")),
+        (list("rlidwka"), list("rlidwka")),
+        (list("adiklrw"), list("rlidwka")),
+    ],
+    ids=lambda p: "".join(p),
+)
+def test_normalize(value, expected):
+    got = normalize(value)
     assert got == expected
 
 
-def test_acl_from_args():
-    t = [
-        "system:administrators rlidwka",
-        "system:anyuser rl",
-        "user1 rl",
-        "user2 rl",
-        "user2 rwl",
-        "user2 -l",
-        "user3 +rlidwk",
-        "user4 none",
-    ]
-    expected = """\
-{'system:administrators': ('rlidwka', ''),
- 'system:anyuser': ('rl', ''),
- 'user1': ('rl', ''),
- 'user2': ('rlw', 'l'),
- 'user3': ('rlidwk', '')}\
+@pytest.mark.parametrize("invalid", list("bcmxyzXZY123.,-+=!?@/(){}"))
+def test_normalize_invalid(invalid):
+    with pytest.raises(AssertionError) as e:
+        normalize([invalid])
+    assert "Illegal rights character" in str(e.value)
+
+
+@pytest.mark.parametrize(
+    "value,sign,rights",
+    [
+        # "",  TODO: This test fails!
+        ("r", "+", list("r")),
+        ("rl", "+", list("rl")),
+        ("rlidwka", "+", list("rlidwka")),
+        ("all", "+", list("rlidwkaABCDEFGH")),
+        ("read", "+", list("rl")),
+        ("write", "+", list("rlidwk")),
+        ("none", "+", list()),
+        ("dlikraw", "+", list("rlidwka")),
+        ("rrrrllll", "+", list("rl")),
+        ("+r", "+", list("r")),
+        ("-r", "-", list("r")),
+        ("-all", "-", list("rlidwkaABCDEFGH")),
+        ("-read", "-", list("rl")),
+        ("-write", "-", list("rlidwk")),
+        ("-none", "-", list()),
+    ],
+)
+def test_parse(value, sign, rights):
+    got_sign, got_rights = parse(value)
+    assert got_sign == sign
+    assert got_rights == rights
+
+
+@pytest.mark.parametrize(
+    "value",
+    [
+        list("abc"),
+        list("+XYZ"),
+        list("---"),
+        list("+++"),
+    ],
+)
+def test_parse_invalid(value):
+    with pytest.raises(AssertionError) as e:
+        parse(value)
+    assert "Illegal rights character" in str(e.value)
+
+
+#
+# Tests for the internal helper class.
+#
+
+
+@pytest.mark.parametrize(
+    "args,expected",
+    [
+        # ([], "", "", {}),  # todo
+        (["u r"], {"u": ("r", "")}),
+        (["u r", "v w"], {"u": ("r", ""), "v": ("w", "")}),
+    ],
+)
+def test_acl_class_from_args(args, expected):
+    a = AccessControlList.from_args(*args)
+    assert a.acls == expected
+
+
+def test_acl_class_from_path(process, tmp_path):
+    process(
+        stdout=f"""\
+Access list for {tmp_path}
+Normal rights:
+  system:anyuser rl
+  user1 rlidwk
 """
-    a = AccessControlList.from_args(*t)
-    got = pprint.pformat(a.acls)
-    assert got == expected
+    )
+    expected = {"system:anyuser": ("rl", ""), "user1": ("rlidwk", "")}
+    a = AccessControlList.from_path(tmp_path)
+    assert a.acls == expected
 
 
-def test_acl_contains():
-    t = [
-        "system:administrators rlidwka",
-        "system:anyuser rl",
-        "user1 rl",
-        "user2 rl",
-        "user2 rwl",
-        "user2 -l",
-        "user3 +rlidwk",
-        "user4 none",
-    ]
-    a = AccessControlList.from_args(*t)
-    assert a.contains("system:administrators", "rlidwka")
-    assert a.contains("system:anyuser", "rl")
-    assert a.contains("user1", "+rl")
-    assert a.contains("user2", "rlw")
-    assert a.contains("user2", "-l")
-    assert a.contains("user3", "+rlidwk")
-    assert not a.contains("user4", "none")
+@pytest.mark.parametrize(
+    "args,name,rights,expected",
+    [
+        # ([], "", "", {}),  # todo
+        ([], "u", "r", {"u": ("r", "")}),
+        ([], "u", "-r", {"u": ("", "r")}),
+        ([], "u", "read", {"u": ("rl", "")}),
+        ([], "u", "write", {"u": ("rlidwk", "")}),
+        ([], "u", "all", {"u": ("rlidwkaABCDEFGH", "")}),
+        (["u r"], "v", "w", {"u": ("r", ""), "v": ("w", "")}),
+        (["u rlidwka"], "u", "rlidwk", {"u": ("rlidwka", "")}),
+    ],
+)
+def test_acl_class_add_entry(args, name, rights, expected):
+    a = AccessControlList.from_args(*args)
+    a.add(name, rights)
+    assert a.acls == expected
+
+
+@pytest.mark.parametrize(
+    "args,name,rights,expected",
+    [
+        ([], "u", "r", False),
+        (["u r"], "u", "r", True),
+        (["u r", "v w"], "v", "w", True),
+        (["u read"], "u", "rl", True),
+        (["u read"], "u", "rlidwk", False),
+        (["u rl", "u -a"], "u", "-a", True),
+    ],
+)
+def test_acl_class_contains(args, name, rights, expected):
+    a = AccessControlList.from_args(*args)
+    assert a.contains(name, rights) is expected
+
+
+#
+# Keyword tests.
+#
+
+
+@pytest.mark.parametrize(
+    "path,name,rights,expected",
+    [
+        ("/a/b/c", "u", "r", "fs setacl -dir /a/b/c -acl u r"),
+        ("/a/b/c", "u", "rlidwka", "fs setacl -dir /a/b/c -acl u rlidwka"),
+        ("/a/b/c", "u", "read", "fs setacl -dir /a/b/c -acl u read"),
+        ("/a/b/c", "u", "write", "fs setacl -dir /a/b/c -acl u write"),
+        ("/a/b/c", "u", "none", "fs setacl -dir /a/b/c -acl u none"),
+    ],
+)
+def test_add_access_rights_passes(process, logged, path, name, rights, expected):
+    keywords.add_access_rights(path, name, rights)
+    got = logged.info[0]
+    assert got == f"running: {expected}"
+
+
+@pytest.mark.parametrize(
+    "acls",
+    [
+        ["user1 rlidwk"],
+    ],
+)
+def test_access_control_list_matches_passes(process, tmp_path, acls):
+    process(
+        stdout=f"""\
+Access list for {tmp_path}
+Normal rights:
+  user1 rlidwk
+"""
+    )
+    keywords.access_control_list_matches(tmp_path, *acls)
+
+
+@pytest.mark.parametrize(
+    "acls",
+    [
+        ["user1 rlidwk"],
+    ],
+)
+def test_access_control_list_matches_fails(process, tmp_path, acls):
+    process(
+        stdout=f"""\
+Access list for {tmp_path}
+Normal rights:
+  user2 rlidwk
+"""
+    )
+    with pytest.raises(AssertionError) as e:
+        keywords.access_control_list_matches(tmp_path, *acls)
+    assert "ACLs do not match" in str(e.value)
+
+
+def test_access_control_list_contains_passes(process, tmp_path):
+    process(
+        stdout=f"""\
+Access list for {tmp_path}
+Normal rights:
+  user1 rlidwk
+"""
+    )
+    name = "user1"
+    rights = "rlidwk"
+    keywords.access_control_list_contains(tmp_path, name, rights)
+
+
+def test_access_control_list_contains_fails(process, tmp_path):
+    process(
+        stdout=f"""\
+Access list for {tmp_path}
+Normal rights:
+  user1 rlidwk
+"""
+    )
+    name = "user1"
+    rights = "rl"
+    with pytest.raises(AssertionError) as e:
+        keywords.access_control_list_contains(tmp_path, name, rights)
+    assert "ACL entry rights do not match" in str(e.value)
+
+
+def test_access_control_should_exist_passes(process, tmp_path):
+    process(
+        stdout=f"""\
+Access list for {tmp_path}
+Normal rights:
+  user1 rlidwk
+"""
+    )
+    name = "user1"
+    keywords.access_control_should_exist(tmp_path, name)
+
+
+def test_access_control_should_exist_fails(process, tmp_path):
+    process(
+        stdout=f"""\
+Access list for {tmp_path}
+Normal rights:
+  user1 rlidwk
+"""
+    )
+    name = "user2"
+    with pytest.raises(AssertionError) as e:
+        keywords.access_control_should_exist(tmp_path, name)
+    assert "ACL entry does not exist" in str(e.value)
+
+
+def test_access_control_should_not_exist_passes(process, tmp_path):
+    process(
+        stdout=f"""\
+Access list for {tmp_path}
+Normal rights:
+  user1 rlidwk
+"""
+    )
+    name = "user2"
+    with pytest.raises(AssertionError) as e:
+        keywords.access_control_should_exist(tmp_path, name)
+    assert "ACL entry does not exist" in str(e.value)
+
+
+def test_access_control_should_not_exist_fails(process, tmp_path):
+    process(
+        stdout=f"""\
+Access list for {tmp_path}
+Normal rights:
+  user1 rlidwk
+"""
+    )
+    name = "user1"
+    keywords.access_control_should_exist(tmp_path, name)
